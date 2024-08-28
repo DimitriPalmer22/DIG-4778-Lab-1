3950738d538944e72f57a8aa45f4f75611599590
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2024-08-28 15:13:39 -0400

    Working Piece Selection

---
+++ Assets/_Scripts/ChessPiece.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class ChessPiece : MonoBehaviour
+{
+    public enum PieceType
+    {
+        Pawn,
+        Rook,
+        Knight,
+        Bishop,
+        Queen,
+        King
+    }
+
+    [SerializeField] private Color color;
+    
+    [SerializeField] private PieceType pieceType;
+
+    [Header("Piece Types")]
+    [SerializeField] private GameObject pawnn;
+    [SerializeField] private GameObject rook;
+    [SerializeField] private GameObject knight;
+    [SerializeField] private GameObject bishop;
+    [SerializeField] private GameObject queen;
+    [SerializeField] private GameObject king;
+
+    private GameObject _piece;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn a pawn if the piece is null
+        if (_piece == null)
+            InstantiatePiece(PieceType.Pawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the correct piece is instantiated
+        if (_piece != null && _piece.tag != pieceType.ToString())
+        {
+            DestroyImmediate(_piece);
+            InstantiatePiece(pieceType);
+        }
+        
+    }
+
+    private void InstantiatePiece(PieceType pieceType)
+    {
+        switch (pieceType)
+        {
+            case PieceType.Pawn:
+                _piece = Instantiate(pawnn, transform);
+                break;
+            case PieceType.Rook:
+                _piece = Instantiate(rook, transform);
+                break;
+            case PieceType.Knight:
+                _piece = Instantiate(knight, transform);
+                break;
+            case PieceType.Bishop:
+                _piece = Instantiate(bishop, transform);
+                break;
+            case PieceType.Queen:
+                _piece = Instantiate(queen, transform);
+                break;
+            case PieceType.King:
+                _piece = Instantiate(king, transform);
+                break;
+        }
+    }
+}
